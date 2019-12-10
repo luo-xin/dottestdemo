f581f588f30e92cc9a27a8c038c0218bfaa42879
luoxin <luoxin@fangdd.com>
2019-12-10 20:21:11 +0800

    first commit

--- src/main/java/com/dottestdemo/controller/DotController.java
+++ src/main/java/com/dottestdemo/controller/DotController.java
@@ -1,16 +1,17 @@
 package com.dottestdemo.controller;
 
-import com.dottestdemo.bean.Dot;
 import com.dottestdemo.service.DotService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import com.dottestdemo.utils.ReadJsonFileUtil;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 
 import javax.annotation.Resource;
+import javax.sql.DataSource;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -20,24 +21,32 @@ public class DotController {
     @Resource
     private DotService dotService;
 
+    @Resource
+    @Qualifier("hiveDruidDataSource")
+    DataSource druidDataSource;
 
     @RequestMapping(value = "/check", method = {RequestMethod.POST})
     public @ResponseBody
-    ModelAndView check(Dot dot) {
-        Map<String, Boolean> resultMap = new HashMap<>();
-
-        System.out.println(dot.getDt());
-        System.out.println(dot.getEvent());
-
-        boolean dotRst = false;
+    ModelAndView check(@RequestParam("file") MultipartFile file) {
+        Map<String, Object> resultMap = new HashMap<>();
+        if (file.isEmpty()) {
+            resultMap.put("message", "上传失败，请选择文件");
+            return new ModelAndView(new MappingJackson2JsonView(), resultMap);
+        }
         try {
-            dotRst = dotService.queryEvent(dot);
+            Map<String, Object> mmp = ReadJsonFileUtil.getMap(file.getInputStream());
 
-            resultMap.put("result", dotRst);
+            // Map<String, Object> mmp = ReadJsonFileUtil.getMap("dot.json");
+            //mmp.forEach((k, v) -> {
+            //  System.out.println(k + ": " + v);
+            //});
+
+            // 验证打点
+            List<Map<String, Object>> datas = dotService.queryEvent(mmp);
+
+            resultMap.put("data", datas);
         } catch (Exception e) {
             e.printStackTrace();
-
-            resultMap.put("result", false);
         }
 
         return new ModelAndView(new MappingJackson2JsonView(), resultMap);
